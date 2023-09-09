3042e05d91338b0148197ccf385933027bc83539
lone64 <140879165+lone64Plugins@users.noreply.github.com>
2023-09-09 16:13:59 +0900

    Add files via upload

--- translator-plugin/src/main/java/org/lone64/sktranslator/SkTranslator.java
+++ translator-plugin/src/main/java/org/lone64/sktranslator/SkTranslator.java
@@ -6,13 +6,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SkTranslator extends JavaPlugin {
 
-    private SkriptAddon addon;
-
     @Override
     public void onEnable() {
         try {
-            this.addon = Skript.registerAddon(this);
-            this.addon.loadClasses("org.lone64.sktranslator", "skript");
+            SkriptAddon addon = Skript.registerAddon(this);
+            addon.loadClasses("org.lone64.sktranslator", "skript");
         } catch (Exception ignored) { }
     }
 
